af978a297bceb9ea66c1e33a718955b56704b4fa
Flavio Lima <flaviolima.s@live.com>
2021-06-10 17:43:54 -0300

    instanciando e implementando objeto

--- src/generics/CaixaObjetoTeste.java
+++ src/generics/CaixaObjetoTeste.java
@@ -2,6 +2,8 @@ package generics;
 
 public class CaixaObjetoTeste {
     public static void main(String[] args) {
-        
+        CaixaObjeto caixaA = new CaixaObjeto();
+
+        caixaA.setCoisa(2.3); // double -> Double
     }
 }
