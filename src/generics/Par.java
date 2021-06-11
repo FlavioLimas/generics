14c0c0e335b10a8a07d29023d42ac3441e3c2242
Flavio Lima <flaviolima.s@live.com>
2021-06-11 18:00:03 -0300

    classe generics com atributos geters seters

---
+++ src/generics/Par.java
@@ -0,0 +1,23 @@
+package generics;
+
+public class Par<C, V> {
+    
+    private C chave;
+    private V valor;
+
+    public C getChave() {
+        return chave;
+    }
+
+    public void setChave(C chave) {
+        this.chave = chave;
+    }
+
+    public V getValor() {
+        return valor;
+    }
+
+    public void setValor(V valor) {
+        this.valor = valor;
+    }
+}
