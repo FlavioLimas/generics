454ae1a1052bb034ceeba3a9f36fcc6d2ece8ab7
Flavio Lima <flaviolima.s@live.com>
2021-06-11 16:44:50 -0300

    Implementando retorno totalmente generico

--- src/generics/ListaUtil.java
+++ src/generics/ListaUtil.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class ListaUtil {
     
-    public static Object getLastItem(List<?> lista) {
+    public static Object getLastItem1(List<?> lista) {
         return lista.get(lista.size() -1);
     }
 
-    public static <T> T getLastItemNew(List<T> lista) {
+    public static <T> T getLastItem2(List<T> lista) {
         return lista.get(lista.size() -1);
     }
 }
--- src/generics/ListaUtilTeste.java
+++ src/generics/ListaUtilTeste.java
@@ -8,10 +8,16 @@ public class ListaUtilTeste {
         List<String> langs = Arrays.asList("JS", "PHP", "Java", "C++");
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-        String ultimaLinguagem = (String) ListaUtil.getLastItem(langs);
-        System.out.println(ultimaLinguagem);
+        String ultimaLinguagem1 = (String) ListaUtil.getLastItem1(langs);
+        System.out.println(ultimaLinguagem1);
 
-        Integer ultimoNumero = (Integer) ListaUtil.getLastItem(nums);
-        System.out.println(ultimoNumero);
+        Integer ultimoNumero1 = (Integer) ListaUtil.getLastItem1(nums);
+        System.out.println(ultimoNumero1);
+        
+        String ultimaLinguagem2 = ListaUtil.getLastItem2(langs);
+        System.out.println(ultimaLinguagem2);
+
+        Integer ultimoNumero2 = ListaUtil.getLastItem2(nums);
+        System.out.println(ultimoNumero2);
     }
 }
