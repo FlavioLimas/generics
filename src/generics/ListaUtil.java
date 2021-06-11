da2d3efe4aef5515e83c980ff4ae96e0708f13e6
Flavio Lima <flaviolima.s@live.com>
2021-06-11 16:41:15 -0300

    definindo o tipo de retorno do metodo como generic

--- src/generics/ListaUtil.java
+++ src/generics/ListaUtil.java
@@ -7,4 +7,8 @@ public class ListaUtil {
     public static Object getLastItem(List<?> lista) {
         return lista.get(lista.size() -1);
     }
+
+    public static <T> T getLastItemNew(List<T> lista) {
+        return lista.get(lista.size() -1);
+    }
 }
