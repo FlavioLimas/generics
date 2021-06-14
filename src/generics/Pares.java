5e6c07926c3e2ffbafdeb641c3a607945928620d
Flavio Lima <flaviolima.s@live.com>
2021-06-14 18:26:42 -0300

    substituicao do valor informado caso ele se repita

--- src/generics/Pares.java
+++ src/generics/Pares.java
@@ -8,7 +8,12 @@ public class Pares<C extends Number, V> {
     private final Set<Par<C, V>> itens = new TreeSet<>();
 
     public void adicionar(C chave, V valor) {
-        if(chave == null) return;
-        itens.add(new Par<C, V>(chave, valor));
+        if(chave == null) return;  // = null para a execução
+        Par<C, V> novoPar = new Par<C, V>(chave, valor);
+
+        if(itens.contains(novoPar)) { // tem um valor igual no set?
+            itens.remove(novoPar); // remove
+        }
+        itens.add(novoPar); // add novo valor OU SEJA atualiza a chave
     }
 }
