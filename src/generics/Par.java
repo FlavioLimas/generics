32c79bbb90f3f7f5b83f6416338a4282b4ad63cd
Flavio Lima <flaviolima.s@live.com>
2021-06-14 17:58:30 -0300

    implementando equals e hashCode

--- src/generics/Par.java
+++ src/generics/Par.java
@@ -29,4 +29,29 @@ public class Par<C, V> {
     public void setValor(V valor) {
         this.valor = valor;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((chave == null) ? 0 : chave.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Par other = (Par) obj;
+        if (chave == null) {
+            if (other.chave != null)
+                return false;
+        } else if (!chave.equals(other.chave))
+            return false;
+        return true;
+    }
 }
