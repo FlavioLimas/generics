dde01d22511ab121d2fdc1a962791c8686a07a7c
Flavio Lima <flaviolima.s@live.com>
2021-06-14 18:15:24 -0300

    implementando set em metodo adicionar

--- src/generics/Pares.java
+++ src/generics/Pares.java
@@ -3,7 +3,12 @@ package generics;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class Pares<X, Y> {
+public class Pares<C extends Number, V> {
     
-    private final Set<Par<X, Y>> intens = new TreeSet<>();
+    private final Set<Par<C, V>> itens = new TreeSet<>();
+
+    public void adicionar(C chave, V valor) {
+        
+        itens.add(new Par<C, V>(chave, valor));
+    }
 }
