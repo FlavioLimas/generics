df7c6432e8dc780d02a29e85d59439c1b26ed369
Flavio Lima <flaviolima.s@live.com>
2021-06-15 16:45:30 -0300

    correcao de coments

--- src/generics/Pares.java
+++ src/generics/Pares.java
@@ -9,7 +9,7 @@ public class Pares<C extends Number, V> {
     private final Set<Par<C, V>> itens = new HashSet<>();
 
     public void adicionar(C chave, V valor) {
-        if(chave == null) return;  // = null para a execução
+        if(chave == null) return;  // = null parar a execução
         Par<C, V> novoPar = new Par<C, V>(chave, valor);
 
         if(itens.contains(novoPar)) { // tem um valor igual no set?
