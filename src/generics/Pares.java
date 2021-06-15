dcb777a8dfdb1825326d020f492c029967fc1a37
Flavio Lima <flaviolima.s@live.com>
2021-06-15 14:59:46 -0300

    alterando estrutura para compara elemento

--- src/generics/Pares.java
+++ src/generics/Pares.java
@@ -1,12 +1,12 @@
 package generics;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Pares<C extends Number, V> {
     
-    private final Set<Par<C, V>> itens = new TreeSet<>();
+    private final Set<Par<C, V>> itens = new HashSet<>();
 
     public void adicionar(C chave, V valor) {
         if(chave == null) return;  // = null para a execução
