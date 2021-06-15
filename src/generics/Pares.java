01762f0d278d76da705b305364d787fe8b2f7ca4
Flavio Lima <flaviolima.s@live.com>
2021-06-15 18:01:50 -0300

    Entendendo a ordenacao do TreeSet e compareble

--- src/generics/Pares.java
+++ src/generics/Pares.java
@@ -3,11 +3,10 @@ package generics;
 import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Pares<C extends Number, V> {
     
-    private final Set<Par<C, V>> itens = new TreeSet<>();
+    private final Set<Par<C, V>> itens = new LinkedHashSet<>();
 
     public void adicionar(C chave, V valor) {
         if(chave == null) return;  // = null parar a execução
